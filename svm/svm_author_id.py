fd847af043c646bab6cfc9abefaca1a9c4b70d5c
Lukas Spranger <lukas.spranger@fau.de>
2016-09-28 11:03:36 +0200

    svm exercise

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -20,11 +22,28 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+clf = SVC(kernel='rbf', C=10000.)
 
+# smaller dataset
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
-#########################################################
-### your code goes here ###
+print "fitting the model."
 
-#########################################################
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+t0 = time()
+predictions = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
 
+print predictions
+
+print accuracy_score(labels_test, predictions)
+
+print predictions[10]
+print predictions[26]
+print predictions[50]
+
+print predictions.sum()
